9d74e89d48aa607529b79acc5d4d129ddc4bd3d7
Breno Barbosa <brb95_@hotmail.com>
2023-09-11 08:42:42 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from "./Components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu primeiro projeto React</h1>
+      <Button tittle="Entrar"/>
+    </div>
+  );
+}
+
+export default App;
